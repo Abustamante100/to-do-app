2716ad85d1a3913dd83c82da195888a0f38ccadf
Arnold <ArnoldBustamante@Gmail.com>
2020-04-27 11:30:09 -0400

    first commit

--- app.js
+++ app.js
@@ -1,47 +1,45 @@
 function onReady() {
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', () => {
-     event.preventDefault()
-
-    // get the text
-    let title = newToDoText.value;
-
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
     newToDoText.value = '';
 
-    //delete button
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
-    newLi.appendChild(deleteBtn);
+    renderTheUI();
+  }
 
-    deleteBtn.addEventListener('click', function(){
-      toDoList.removeChild(this.parentElement);
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+    if (!newToDoText.value) { return; }
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
     });
 
-  });
-}
+  }
 
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+  });
+
+  renderTheUI();
+
+}
 
 window.onload = function() {
   onReady();
